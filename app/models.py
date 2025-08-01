51a772038374bf99ad1c0210c1000637386bd8bd
Sgorilla3v3 <khu8276@gmail.com>
2025-08-01 13:42:41 +0900

    Update models.py
    add| class

--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
 # app/models.py
 # Pydantic 모델 및 DB 스키마를 정의하는 곳
 from pydantic import BaseModel
+from datetime import datetime
 
 class Post(BaseModel):
     id: str
     caption: str = None
+    username: str
+    timestamp: datetime  # 게시물 업로드 시간
     like_count: int
     comments_count: int
+    permalink: str
+    media_type: str  # IMAGE, VIDEO, CAROUSEL_ALBUM 등
