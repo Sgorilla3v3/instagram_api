47955e0ac9717b0716a4622c68682e99bfe1ebd6
Sgorilla3v3 <khu8276@gmail.com>
2025-08-01 10:19:15 +0900

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
+# app/models.py
+# Pydantic 모델 및 DB 스키마를 정의하는 곳
+from pydantic import BaseModel
 
+class Post(BaseModel):
+    id: str
+    caption: str = None
+    like_count: int
+    comments_count: int
